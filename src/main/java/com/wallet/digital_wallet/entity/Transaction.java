9bd514bb9b454a563d7b3e4db740f04753dc0130
Joshua Maina <dev.joshuamaina@gmail.com>
2026-01-14 09:38:45 +0300

    feat: implemented the entity files

---
+++ src/main/java/com/wallet/digital_wallet/entity/Transaction.java
@@ -0,0 +1,61 @@
+package com.wallet.digital_wallet.entity;
+
+import com.wallet.digital_wallet.enums.TransactionStatus;
+import com.wallet.digital_wallet.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "transaction_id", unique = true, nullable = false)
+    private String transactionId;
+
+    @Column(name = "reference_number", unique = true, nullable = false)
+    private String referenceNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "from_wallet_id")
+    private Wallet fromWallet;
+
+    @ManyToOne
+    @JoinColumn(name = "to_wallet_id")
+    private Wallet toWallet;
+
+    @ManyToOne
+    @JoinColumn(name = "merchant_id")
+    private Merchant merchant;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal amount;
+
+    @Column(precision = 15, scale = 2)
+    private BigDecimal fee =BigDecimal.ZERO;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status = TransactionStatus.PENDING;
+
+    private String description;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
