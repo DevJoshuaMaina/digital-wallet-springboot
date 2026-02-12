f51b8b84d08a1229720fbfbd1ca10332f2682751
Joshua Maina <dev.joshuamaina@gmail.com>
2026-02-12 16:51:25 +0300

    refactor: added code to allow server to run after authorization feature was included

--- src/main/java/com/wallet/digital_wallet/security/SecurityConfig.java
+++ src/main/java/com/wallet/digital_wallet/security/SecurityConfig.java
@@ -42,9 +42,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
