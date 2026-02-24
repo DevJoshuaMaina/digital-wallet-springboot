33b3f221985fffd455d3723b1922a3c41523d6bb
Joshua Maina <dev.joshuamaina@gmail.com>
2026-02-24 09:09:50 +0300

    chore: configure for production deployment

--- src/main/java/com/wallet/digital_wallet/config/OpenApiConfig.java
+++ src/main/java/com/wallet/digital_wallet/config/OpenApiConfig.java
@@ -1,6 +1,5 @@
 package com.wallet.digital_wallet.config;
 
-import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
@@ -9,8 +8,6 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 /**
  * OpenAPI/Swagger configuration for the Digital Wallet System.
@@ -42,7 +39,6 @@ public class OpenApiConfig {
                                         .name("bearerAuth")
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .servers(List.of(new Server().url("http://localhost:8080").description("Local Server")));
+                                        .bearerFormat("JWT")));
     }
 }
